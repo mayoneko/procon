da23fb2649a7900cd6146351af8905adfeb27ecf
mayoneko <ev60622@meiji.ac.jp>
2019-02-24 23:38:58 +0900

    resolve with model answer

--- src/19-02-24-C.kt
+++ src/19-02-24-C.kt
@@ -3,63 +3,24 @@
 fun main(args: Array<String>) {
     val (n, a, b, c) = readLine()!!.split(" ").map(String::toInt)
     val ls = mutableListOf<Int>()
-    val ss = listOf(a, b, c)
     for (i in 1..n) {
         ls.add(readLine()!!.toInt())
     }
-
-
+    println(dfa(n, ls, a, b, c, 0, 0, 0, 0))
 }
 
-
-//{
-//    var lsl = ls2lsl(ls)
-//    for (i in 0 until n - 3) {
-//        lsl = lsl.plus(lsMix(lsl))
-//    }
-//    ss.forEach {
-//        println(nearestSearch(it, lsl))
-//    }
-//}
-
-fun ls2lsl(ls: List<Int>): List<Pair<Int, List<Int>>> {
-    return ls.mapIndexed { id, it ->
-        it to listOf(id)
-    }
-}
-
-fun nearestSearch(a: Int, lsl: List<Pair<Int, List<Int>>>): List<Pair<Int, List<Int>>> {
-    return lsl.sortedBy {
-        Math.abs(it.first - a)
-    }
-}
-
-fun lsMix(lsl: List<Pair<Int, List<Int>>>): List<Pair<Int, List<Int>>> {
-    val lil = mutableListOf<Pair<Int, List<Int>>>()
-    for (i in 0 until lsl.size) {
-        for (j in 1 until lsl.size) {
-            if (lsl[i].second.any { lsli -> lsl[j].second.any { it <= lsli } }) continue
-            lil.add(lsl[i].first + lsl[j].first to lsl[i].second.plus(lsl[j].second))
+fun dfa(n: Int, l: List<Int>, A: Int, B: Int, C: Int, cur: Int, a: Int, b: Int, c: Int): Int {
+    if (cur == n) {
+        return if (listOf(a, b, c).min()!! > 0) {
+            Math.abs(a - A) + Math.abs(b - B) + Math.abs(c - C) - 30
+        } else {
+            10000000
         }
     }
-    return lil
-}
-
-
-fun addSearch(a: Int, ls: List<Int>): List<Int> {
-    return ls.filter {
-        it in a - 9..a + 9
-    }
-}
-
-fun joinSearch(a: Int, ls: List<Int>): List<Pair<Int, Int>> {
-    val li = mutableListOf<Pair<Int, Int>>()
-    for (i in 0 until ls.size) {
-        for (j in 1 until ls.size) {
-            if (i >= j) continue
-            if (ls[i] + ls[j] == a) li.add(ls[i] to ls[j])
-        }
-    }
-    return li
+    val ret0 = dfa(n, l, A, B, C, cur + 1, a, b, c)
+    val ret1 = dfa(n, l, A, B, C, cur + 1, a + l[cur], b, c) + 10
+    val ret2 = dfa(n, l, A, B, C, cur + 1, a, b + l[cur], c) + 10
+    val ret3 = dfa(n, l, A, B, C, cur + 1, a, b, c + l[cur]) + 10
+    return listOf(ret0, ret1, ret2, ret3).min()!!
 }
 
